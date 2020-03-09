fc52dff67ea999da43a1c13495d38d0e68d810e1
Babiy <olegbabiy77@gmail.com>
2020-03-09 20:14:00 +0200

    Added library slick.

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,14 +1,16 @@
 import React from "react";
 import classes from "./Portfolio.module.scss";
 
-import dog from "../../assests/dogs/dog1.png";
+import dog from "../../assests/pets/dog1.png";
+import dog2 from "../../assests/pets/dog2.png";
+
 import Button from "../UI/Button/Button";
 
 const Portfolio = () => {
   return (
     <div className={classes.Portfolio}>
       <div className={classes.ImageContainer}>
-        <img className={classes.Image} src={dog} alt="pet" />
+        <img className={classes.Image} src={dog2} alt="pet" />
       </div>
       <div className={classes.Content}>
         <h4 className={classes.Title}>Jennifer</h4>
--- src/components/sections/Friends/Friends.js
+++ src/components/sections/Friends/Friends.js
@@ -1,10 +1,26 @@
 import React from "react";
 import classes from "./Friends.module.scss";
 
+import dog1 from "../../../assests/pets/dog1.png";
+import dog2 from "../../../assests/pets/dog2.png";
+import dog3 from "../../../assests/pets/dog3.png";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import Portfolio from "../../Portfolio/Portfolio";
 import Button from "../../UI/Button/Button";
 
 const Friends = props => {
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500
+  };
   return (
     <div className={classes.Friends}>
       <div className={classes.Container}>
@@ -12,7 +28,17 @@ const Friends = props => {
           Our friends who <br /> are looking for a house
         </h4>
         <div className={classes.Content}>
-          <Portfolio />
+          <Slider {...settings}>
+            <Portfolio />
+            <Portfolio />
+            <Portfolio />
+            <Portfolio />
+            <Portfolio />
+            <Portfolio />
+            {/* <img className={classes.Image} src={dog1} alt="" />
+            <img className={classes.Image} src={dog2} alt="" />
+            <img className={classes.Image} src={dog3} alt="" /> */}
+          </Slider>
         </div>
         <div className={classes.ButtonContainer}>
           <Button type="main">Get to know the rest</Button>
