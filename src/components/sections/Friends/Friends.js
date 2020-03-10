f7ef57f56209c4946d2cece5e78aa8ab646111ec
Babiy <olegbabiy77@gmail.com>
2020-03-10 15:50:20 +0200

    Deleted Slick.

--- src/components/sections/Friends/Friends.js
+++ src/components/sections/Friends/Friends.js
@@ -5,41 +5,17 @@ import dog1 from "../../../assests/pets/dog1.png";
 import dog2 from "../../../assests/pets/dog2.png";
 import dog3 from "../../../assests/pets/dog3.png";
 
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 import Portfolio from "../../Portfolio/Portfolio";
 import Button from "../../UI/Button/Button";
 
 const Friends = props => {
-  const settings = {
-    className: "center",
-    centerMode: true,
-    infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
-    speed: 500
-  };
   return (
     <div className={classes.Friends}>
       <div className={classes.Container}>
         <h4 className={classes.Title}>
           Our friends who <br /> are looking for a house
         </h4>
-        <div className={classes.Content}>
-          <Slider {...settings}>
-            <Portfolio />
-            <Portfolio />
-            <Portfolio />
-            <Portfolio />
-            <Portfolio />
-            <Portfolio />
-            {/* <img className={classes.Image} src={dog1} alt="" />
-            <img className={classes.Image} src={dog2} alt="" />
-            <img className={classes.Image} src={dog3} alt="" /> */}
-          </Slider>
-        </div>
+        <div className={classes.Content}></div>
         <div className={classes.ButtonContainer}>
           <Button type="main">Get to know the rest</Button>
         </div>
